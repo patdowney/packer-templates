773a097bc588ae316c0b94f083dbfe82690c749e
Pat Downey <pat.downey@gmail.com>
2016-04-30 19:12:33 +0100

    i think the debian folks know there shit better than me, so update to use their config, with the sysvinit changes

--- debian-jessie64-sysvinit/scripts/cleanup.sh
+++ debian-jessie64-sysvinit/scripts/cleanup.sh
@@ -1,14 +1,10 @@
-apt-get -y autoremove
-apt-get -y clean
+# Clean up
+apt-get --yes autoremove
+apt-get --yes clean
 
-echo "cleaning up guest additions"
-rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
-
+# Removing leftover leases and persistent rules
 echo "cleaning up dhcp leases"
 rm /var/lib/dhcp/*
 
-echo "cleaning up udev rules"
-rm /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
-rm -rf /dev/.udev/
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
+echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
+echo "pre-up sleep 2" >> /etc/network/interfaces
--- debian-jessie64-sysvinit/scripts/vagrant.sh
+++ debian-jessie64-sysvinit/scripts/vagrant.sh
@@ -1,8 +1,13 @@
+# Set up Vagrant.
+
 date > /etc/vagrant_box_build_time
 
-mkdir /home/vagrant/.ssh
-wget --no-check-certificate \
-    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
-    -O /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
-chmod -R go-rwsx /home/vagrant/.ssh
+# Install vagrant keys
+mkdir -pm 700 /home/vagrant/.ssh
+curl -Lo /home/vagrant/.ssh/authorized_keys \
+  'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+# Customize the message of the day
+echo 'Welcome to your Vagrant-built virtual machine.' > /var/run/motd
